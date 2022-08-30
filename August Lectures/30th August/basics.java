84ef1e8c0333bcd31ebde4507250b9993ff69e25
Pravin <pravincoding49@gmail.com>
2022-08-30 18:25:18 +0530

    30th August

--- August Lectures/30th August/basics.java
+++ August Lectures/30th August/basics.java
@@ -1,5 +1,31 @@
 public class basics {
     public static void main(String[] args) {
+        /* int ageOfKiranAsOf2022IsThis = 20;
+        // Camel case.
+        int y = 30;
         
+        // Print statement: To display some message.
+        System.out.println(ageOfKiranAsOf2022IsThis);
+        System.out.println(y); */
+
+        // double quotes.
+        String intro = "My name is Kiran";
+
+        double z = 5.67;
+
+        char s = '@';
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long d = 13134134;
     }
+
+    // Abstraction: 
+    
+    // {} ==> Curly braces.
+    // [] ==> square brackets.
+    // () ==> parentheses.
+    
+    // Compiler.
 }
