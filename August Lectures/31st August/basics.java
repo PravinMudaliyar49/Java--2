20f200a5eb45e70ab48acf006843740e03573921
Pravin <pravincoding49@gmail.com>
2022-08-31 12:55:43 +0530

    31st August

---
+++ August Lectures/31st August/basics.java
@@ -0,0 +1,35 @@
+public class basics {
+    public static void main(String[] args) {
+        // This is my age:      ... Single line comment
+        int age = 20;
+
+        /* alsfkdafs        ... multi line comment.
+        adsfjkad
+        aksdfafds
+        akdsfbjdf
+        adksfjdasf
+        aksdfbfdka
+        sljdv
+        asdvoisdav
+        asdivj
+        */
+
+        // Concatenation:
+
+        // String description = "This is my age";
+        // int age = 20;
+        // char dot = '.';
+
+        // System.out.println(description + " " + age + dot);
+
+        // This is my age 20.
+        System.out.println("This is my age" + " " + 20 + '.');
+
+        // Z-pattern:
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
